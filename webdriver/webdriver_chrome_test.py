4f994a77cb46829ab03e991ae63808f2c61642a8
emedentsii <emedentsii@gmail.com>
2016-05-30 17:56:23 -0300

    Chrome test update with example of browser option.

--- webdriver/webdriver_chrome_test.py
+++ webdriver/webdriver_chrome_test.py
@@ -1,5 +1,6 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # test
 display = Display(visible=0, size=(800, 600))
@@ -8,7 +9,9 @@ display.start()
 # now Firefox will run in a virtual display. 
 # you will not see the browser.
 try:
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.get('http://www.google.com')
     print(browser.title)
     browser.quit()
